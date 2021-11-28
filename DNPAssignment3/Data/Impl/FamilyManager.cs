a526634efb8701e7191a7d8d7e28b6fa087128b3
Young <andreasyoungandersen@gmail.com>
2021-11-28 18:57:32 +0100

    Added a ROOT variable

--- DNPAssignment3/Data/Impl/FamilyManager.cs
+++ DNPAssignment3/Data/Impl/FamilyManager.cs
@@ -9,155 +9,157 @@ using Models;
 
 namespace Data.Impl
 {
-    public class FamilyManager : IFamilyManager
-    {
-        public async Task AddFamily(Family family)
-        {
-            using HttpClient client = new();
+	public class FamilyManager : IFamilyManager
+	{
+		private readonly string ROOT = "https://localhost:5003";
 
-            string familyAsJson = JsonSerializer.Serialize(family);
+		public async Task AddFamily(Family family)
+		{
+			using HttpClient client = new();
 
-            StringContent content = new StringContent(
-            familyAsJson,
-            Encoding.UTF8,
-            "application/json"
-                );
-            HttpResponseMessage responseMessage = await client.PostAsync($"https://localhost:5003/FamilyService", content);
-            if (!responseMessage.IsSuccessStatusCode)
-                throw new Exception($"Error: {responseMessage.StatusCode}, {responseMessage.ReasonPhrase}");
+			string familyAsJson = JsonSerializer.Serialize(family);
 
-        }
+			StringContent content = new StringContent(
+			familyAsJson,
+			Encoding.UTF8,
+			"application/json"
+				);
+			HttpResponseMessage responseMessage = await client.PostAsync($"{ROOT}/FamilyService", content);
+			if (!responseMessage.IsSuccessStatusCode)
+				throw new Exception($"Error: {responseMessage.StatusCode}, {responseMessage.ReasonPhrase}");
 
-        public async Task RemoveFamily(Family family)
-        {
-            using HttpClient client = new();
-            HttpResponseMessage responseMessage = await client.DeleteAsync($"https://localhost:5003/FamilyService/{family.FamilyId}");
-            if (!responseMessage.IsSuccessStatusCode)
-                throw new Exception($"Error: {responseMessage.StatusCode}, {responseMessage.ReasonPhrase}");
-        }
+		}
 
-        public async Task<IList<Family>> GetFamilies()
-        {
-            using HttpClient client = new();
+		public async Task RemoveFamily(Family family)
+		{
+			using HttpClient client = new();
+			HttpResponseMessage responseMessage = await client.DeleteAsync($"{ROOT}/FamilyService/{family.FamilyId}");
+			if (!responseMessage.IsSuccessStatusCode)
+				throw new Exception($"Error: {responseMessage.StatusCode}, {responseMessage.ReasonPhrase}");
+		}
 
-            HttpResponseMessage responseMessage = await client.GetAsync("https://localhost:5003/FamilyService");
+		public async Task<IList<Family>> GetFamilies()
+		{
+			using HttpClient client = new();
 
-            if (!responseMessage.IsSuccessStatusCode)
-                throw new Exception($"{responseMessage.StatusCode}, {responseMessage.ReasonPhrase}");
+			HttpResponseMessage responseMessage = await client.GetAsync("{ROOT}/FamilyService");
 
-            string result = await responseMessage.Content.ReadAsStringAsync();
+			if (!responseMessage.IsSuccessStatusCode)
+				throw new Exception($"{responseMessage.StatusCode}, {responseMessage.ReasonPhrase}");
 
-            IList<Family> families = JsonSerializer.Deserialize<IList<Family>>(result, new JsonSerializerOptions
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            });
+			string result = await responseMessage.Content.ReadAsStringAsync();
 
-            return families;
-            
-        }
+			IList<Family> families = JsonSerializer.Deserialize<IList<Family>>(result, new JsonSerializerOptions
+			{
+				PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+			});
 
-        public async Task UpdateFamily(Family family)
-        {
-            using HttpClient client = new();
+			return families;
 
-            string familyAsJson = JsonSerializer.Serialize(family); 
+		}
 
-            StringContent content = new StringContent(
-                familyAsJson,
-                Encoding.UTF8,
-                "application/json"
-            );
-            HttpResponseMessage responseMessage = 
-                await client.PatchAsync($"https://localhost:5003/FamilyService/UpdateFamily", content);
-            
-            if (!responseMessage.IsSuccessStatusCode)
-                throw new Exception($"Error: {responseMessage.StatusCode}, {responseMessage.ReasonPhrase}");
-        }
+		public async Task UpdateFamily(Family family)
+		{
+			using HttpClient client = new();
 
-        public async Task<Family> GetFamily(int familyId)
-        {
-            using HttpClient client = new();
+			string familyAsJson = JsonSerializer.Serialize(family);
 
-            HttpResponseMessage responseMessage = await client.GetAsync($"https://localhost:5003/FamilyService/{familyId}");
+			StringContent content = new StringContent(
+				familyAsJson,
+				Encoding.UTF8,
+				"application/json"
+			);
+			HttpResponseMessage responseMessage =
+				await client.PatchAsync($"{ROOT}/FamilyService/UpdateFamily", content);
 
-            if (!responseMessage.IsSuccessStatusCode)
-                throw new Exception($"{responseMessage.StatusCode}, {responseMessage.ReasonPhrase}");
+			if (!responseMessage.IsSuccessStatusCode)
+				throw new Exception($"Error: {responseMessage.StatusCode}, {responseMessage.ReasonPhrase}");
+		}
 
-            string result = await responseMessage.Content.ReadAsStringAsync();
+		public async Task<Family> GetFamily(int familyId)
+		{
+			using HttpClient client = new();
 
-            Family family = JsonSerializer.Deserialize<Family>(result, new JsonSerializerOptions
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            });
+			HttpResponseMessage responseMessage = await client.GetAsync($"{ROOT}/FamilyService/{familyId}");
 
-            return family;
-        }
+			if (!responseMessage.IsSuccessStatusCode)
+				throw new Exception($"{responseMessage.StatusCode}, {responseMessage.ReasonPhrase}");
 
-        public async Task RemoveAdult(int adultId)
-        {
-            using HttpClient client = new();
-            HttpResponseMessage responseMessage = await client.DeleteAsync($"https://localhost:5003/FamilyService/adult/{adultId}");
-            if (!responseMessage.IsSuccessStatusCode)
-                throw new Exception($"Error: {responseMessage.StatusCode}, {responseMessage.ReasonPhrase}");
-        }
+			string result = await responseMessage.Content.ReadAsStringAsync();
 
-        public async Task<IList<Adult>> GetAdults()
-        {
-            using HttpClient client = new();
+			Family family = JsonSerializer.Deserialize<Family>(result, new JsonSerializerOptions
+			{
+				PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+			});
 
-            HttpResponseMessage responseMessage = await client.GetAsync("https://localhost:5003/FamilyService/adults");
+			return family;
+		}
 
-            if (!responseMessage.IsSuccessStatusCode)
-                throw new Exception($"{responseMessage.StatusCode}, {responseMessage.ReasonPhrase}");
+		public async Task RemoveAdult(int adultId)
+		{
+			using HttpClient client = new();
+			HttpResponseMessage responseMessage = await client.DeleteAsync($"{ROOT}/FamilyService/adult/{adultId}");
+			if (!responseMessage.IsSuccessStatusCode)
+				throw new Exception($"Error: {responseMessage.StatusCode}, {responseMessage.ReasonPhrase}");
+		}
 
-            string result = await responseMessage.Content.ReadAsStringAsync();
+		public async Task<IList<Adult>> GetAdults()
+		{
+			using HttpClient client = new();
 
-            IList<Adult> adults = JsonSerializer.Deserialize<IList<Adult>>(result, new JsonSerializerOptions
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            });
-        
-            return adults;
-        }
+			HttpResponseMessage responseMessage = await client.GetAsync("{ROOT}/FamilyService/adults");
 
-        public async Task<Adult> GetAdult(int id)
-        {
-            using HttpClient client = new();
+			if (!responseMessage.IsSuccessStatusCode)
+				throw new Exception($"{responseMessage.StatusCode}, {responseMessage.ReasonPhrase}");
 
-            HttpResponseMessage responseMessage = await client.GetAsync($"https://localhost:5003/FamilyService/adult/{id}");
+			string result = await responseMessage.Content.ReadAsStringAsync();
 
-            if (!responseMessage.IsSuccessStatusCode)
-            {
-                throw new Exception($"{responseMessage.StatusCode}, {responseMessage.ReasonPhrase}");
-            }
+			IList<Adult> adults = JsonSerializer.Deserialize<IList<Adult>>(result, new JsonSerializerOptions
+			{
+				PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+			});
 
-            string result = await responseMessage.Content.ReadAsStringAsync();
+			return adults;
+		}
 
-            Adult adult = JsonSerializer.Deserialize<Adult>(result, new JsonSerializerOptions
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            });
+		public async Task<Adult> GetAdult(int id)
+		{
+			using HttpClient client = new();
 
-            return adult;
-        }
+			HttpResponseMessage responseMessage = await client.GetAsync($"{ROOT}/FamilyService/adult/{id}");
 
-        public async Task UpdateAdult(Adult adult)
-        {
-            using HttpClient client = new();
+			if (!responseMessage.IsSuccessStatusCode)
+			{
+				throw new Exception($"{responseMessage.StatusCode}, {responseMessage.ReasonPhrase}");
+			}
 
-            string adultAsJson = JsonSerializer.Serialize(adult); 
-            StringContent content = new StringContent(
-                adultAsJson,
-                Encoding.UTF8,
-                "application/json"
-            );
-            
-            HttpResponseMessage responseMessage = 
-                await client.PatchAsync($"https://localhost:5003/FamilyService/UpdateAdult", content);
-            
-            if (!responseMessage.IsSuccessStatusCode)
-                throw new Exception($"Error: {responseMessage.StatusCode}, {responseMessage.ReasonPhrase}");
-            
-        }
-    }
+			string result = await responseMessage.Content.ReadAsStringAsync();
+
+			Adult adult = JsonSerializer.Deserialize<Adult>(result, new JsonSerializerOptions
+			{
+				PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+			});
+
+			return adult;
+		}
+
+		public async Task UpdateAdult(Adult adult)
+		{
+			using HttpClient client = new();
+
+			string adultAsJson = JsonSerializer.Serialize(adult);
+			StringContent content = new StringContent(
+				adultAsJson,
+				Encoding.UTF8,
+				"application/json"
+			);
+
+			HttpResponseMessage responseMessage =
+				await client.PatchAsync($"{ROOT}/FamilyService/UpdateAdult", content);
+
+			if (!responseMessage.IsSuccessStatusCode)
+				throw new Exception($"Error: {responseMessage.StatusCode}, {responseMessage.ReasonPhrase}");
+
+		}
+	}
 }
