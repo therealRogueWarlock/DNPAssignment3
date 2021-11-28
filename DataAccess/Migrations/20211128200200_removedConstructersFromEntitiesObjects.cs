3f4c05aa267512e3333fc450f09b62edba781b54
SanderKirchert <Sanders@post.cybercity.dk>
2021-11-28 21:35:29 +0100

    push migrations

---
+++ DataAccess/Migrations/20211128200200_removedConstructersFromEntitiesObjects.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Blazor.Migrations
+{
+    public partial class removedConstructersFromEntitiesObjects : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
